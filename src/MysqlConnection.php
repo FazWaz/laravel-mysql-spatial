dd4422df0b811908817af5907acc213ea98d7396
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2024-04-16 10:14:14 -0400

    feat: remove doctrine mapping

--- src/MysqlConnection.php
+++ src/MysqlConnection.php
@@ -2,7 +2,6 @@
 
 namespace Limenet\LaravelMysqlSpatial;
 
-use Doctrine\DBAL\Types\Type as DoctrineType;
 use Illuminate\Database\MySqlConnection as IlluminateMySqlConnection;
 use Limenet\LaravelMysqlSpatial\Schema\Builder;
 use Limenet\LaravelMysqlSpatial\Schema\Grammars\MySqlGrammar;
@@ -12,25 +11,6 @@ class MysqlConnection extends IlluminateMySqlConnection
     public function __construct($pdo, $database = '', $tablePrefix = '', array $config = [])
     {
         parent::__construct($pdo, $database, $tablePrefix, $config);
-
-        if (class_exists(DoctrineType::class)) {
-            // Prevent geometry type fields from throwing a 'type not found' error when changing them
-            $geometries = [
-                'geometry',
-                'point',
-                'linestring',
-                'polygon',
-                'multipoint',
-                'multilinestring',
-                'multipolygon',
-                'geometrycollection',
-                'geomcollection',
-            ];
-            $dbPlatform = $this->getDoctrineConnection()->getDatabasePlatform();
-            foreach ($geometries as $type) {
-                $dbPlatform->registerDoctrineTypeMapping($type, 'string');
-            }
-        }
     }
 
     /**
